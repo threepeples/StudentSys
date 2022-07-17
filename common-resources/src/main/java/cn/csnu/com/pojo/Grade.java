6fb797c26f9d3516597b552cae7494c7496af0f3
HJW <405316771@qq.com>
2022-07-17 17:46:58 +0800

    基础框架

---
+++ common-resources/src/main/java/cn/csnu/com/pojo/Grade.java
@@ -0,0 +1,38 @@
+package cn.csnu.com.pojo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 年级表（专业下属年级）
+ * </p>
+ *
+ * @author su_jue
+ * @since 2022-07-17
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ApiModel(value="Grade对象", description="年级表（专业下属年级）")
+public class Grade implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "年级id(入学学年+专业号码)")
+    private Integer grade_id;
+
+    @ApiModelProperty(value = "年级人数")
+    private Integer number;
+
+    @ApiModelProperty(value = "专业id")
+    private Integer majors_id;
+
+    @ApiModelProperty(value = "目前处于第几学期")
+    private Integer semester;
+
+
+}
---
+++ common-resources/src/main/java/cn/csnu/com/pojo/Teachers.java
@@ -0,0 +1,42 @@
+package cn.csnu.com.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ * 教师表
+ * </p>
+ *
+ * @author su_jue
+ * @since 2022-07-17
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ApiModel(value="Teachers对象", description="教师表")
+public class Teachers implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "教师编号")
+    @TableId(value = "teacher_id", type = IdType.AUTO)
+    private Integer teacher_id;
+
+    @ApiModelProperty(value = "教师名字")
+    private String name;
+
+    @ApiModelProperty(value = "教师简介")
+    private String description;
+
+    @ApiModelProperty(value = "创建时间")
+    private LocalDateTime create_time;
+
+
+}
