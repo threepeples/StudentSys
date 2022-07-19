b0401d4d404cac9c88bd2ccbebdf4ab56fe0eb55
CSL <2583349042@qq.com>
2022-07-19 15:16:02 +0800

    蔡粟来7.19

--- microservice-course/src/main/java/cn/scnu/com/microservicecourse/mapper/CourseMapper.java
+++ microservice-course/src/main/java/cn/scnu/com/microservicecourse/mapper/CourseMapper.java
@@ -7,5 +7,17 @@ package cn.scnu.com.microservicecourse.mapper;
  * @Version 1.0
  * @Author HJW
  */
+
 public interface CourseMapper {
+    //增加课程
+    public void CoursesSave(Courses courses);
+    //删除单条课程
+    public void DelCourses(Integer courses_id);
+    //更新课程
+    public void CourseUpdate(Courses courses);
+    //查询单条课程
+    public  Courses queryById(Integer courses_id);
+    //查询全部课程
+    public Integer queryTotal();
+
 }
--- microservice-course/src/main/java/cn/scnu/com/microservicecourse/service/CourseService.java
+++ microservice-course/src/main/java/cn/scnu/com/microservicecourse/service/CourseService.java
@@ -11,4 +11,22 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class CourseService {
+    @Autowired
+    private CoursesMapper coursesMapper;//注入CoursesMapper
+    //直接注入JedisCluster,在方法中就可以实现缓存redis数据库使用。
+    @Autowired
+    private JedisCluster
+    //增加课程
+    public void CoursesSave(Courses courses){
+
+    }
+    //删除课程
+    public void DelCourses(String courses_id);
+    //修改课程
+    public void CourseUpdate(Courses courses);
+    //查询单条课程
+    public  Courses queryById(String courses_id);
+    //查询全部课程
+    public Integer queryTotal();
+
 }
