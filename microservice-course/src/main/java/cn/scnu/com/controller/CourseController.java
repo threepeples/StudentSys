50c2692c225dc9c5465c1ae9b51cc1e8906cc196
cathuub <15360041431@163.com>
2022-07-26 19:49:41 +0800

    更新选课配置

--- microservice-course/src/main/java/cn/scnu/com/controller/CourseController.java
+++ microservice-course/src/main/java/cn/scnu/com/controller/CourseController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.ResultSet;
-
 /**
  * @ClassName CourseController
  * @Description
